0eadce3dc76718310b4da6464acf4b2b6a6af48a
aannabottini <anna.bottini@alessandrinimainardi.edu.it>
2026-01-26 13:56:23 +0100

    passo19
    query #16, #17, #18, #19

--- news/views.py
+++ news/views.py
@@ -79,23 +79,26 @@ e quindi se pk==None utilizzerai  il metodo objects.all() altrimenti  il metodo 
 
 def queryBase(request):
     #1. Tutti gli articoli scritti da giornalisti di un certo cognome:
-    articoli_cognome = Articolo.objects.filter(giornalista__cognome='Rossi')
+    articoli_cognome = Articolo.objects.filter(giornalista__cognome='Rossi') #filter() ritorna una lista di oggetti, se non ci sono oggetti con cognome rossi, ritornerà una lista vuota
     
     #2. Totale
-    numero_totale_articoli = Articolo.objects.count()
+    numero_totale_articoli = Articolo.objects.count() #count() ritorna il numero di oggetti presenti nel db
     
     #3. Contare il numero di articoli scritti da un giornalista specifico:
-    giornalista_1 = Giornalista.objects.get(id=2)
-    numero_articoli_giornalista_1 = Articolo.objects.filter(giornalista=giornalista_1).count()
+    try:
+        giornalista_1 = Giornalista.objects.get(id=2) # ritorna un oggetto con id=2, se non esiste va nel blocco except
+        numero_articoli_giornalista_1 = Articolo.objects.filter(giornalista=giornalista_1).count()#conta la lunghezza della lista ritornata da filter()
+    except Giornalista.DoesNotExist: #eccezione che può lanciare il metodo .get()
+        numero_articoli_giornalista_1 = 0
 
-    #4. Ordinare gli articoli che non hanno visualizzazioni:
-    articoli_ordinati = Articolo.objects.order_by('-visualizzazioni')
+    #4. Ordinare gli articoli per numero di visualizzazione in modo decrescente:
+    articoli_ordinati = Articolo.objects.order_by('-visualizzazioni') #order_by() ordina i risultati, in questo caso mettendo il - davanti alla condizione li ordina in modo descrescente
 
     #5. Tutti gli articoli che non hanno visualizzazioni:
     articoli_senza_visualizzazioni = Articolo.objects.filter(visualizzazioni=0)
 
     #6. Articolo più visualizzato
-    articolo_piu_visualizzato = Articolo.objects.order_by('-visualizzazioni').first()
+    articolo_piu_visualizzato = Articolo.objects.order_by('-visualizzazioni').first()# first() Restituisce il primo elemento dell'order_by(-visualizzazioni).
 
     #7. Tutti i giornalisti nati dopo una certa data:
     giornalisti_data = Giornalista.objects.filter(anno_nascita__gt=datetime.date(1990, 1, 1))
@@ -117,7 +120,7 @@ def queryBase(request):
     giornalista_anziano = Giornalista.objects.order_by('-anno_nascita').first()
 
     #13. Gli ultimi 5 articoli pubblicati:
-    ultimi = Articolo.objects.order_by('-data_articolo')[:5]
+    ultimi = Articolo.objects.order_by('-data_articolo')[:5] #[:5] limita il risultato ai primi 5
 
     #14. Tutti gli articoli con un certo numero minimo di visualizzazioni:
     articoli_minime_visualizzazioni = Articolo.objects.filter(visualizzazioni__gte=100)
@@ -125,6 +128,21 @@ def queryBase(request):
     #15. Tutti gli articoli che contengono una certa parola nel titolo:
     articoli_parola = Articolo.objects.filter(titolo__icontains='importante')
 
+    #16. Articoli pubblicati in un certo mese di un anno specifico
+    articoli_mese_anno = Articolo.objects.filter(data__month=1, data__year=2023) #restituisce una lista
+
+    #17. Giornalisti con almeno un articolo con più di 100 visualizzazioni
+    giornalisti_con_articoli_popolari = Giornalista.objects.filter(articoli__visualizzazioni__gte=100).distinct
+    
+    data = datetime.date(1990,1,1)
+    visualizzazioni = 50
+    #18. scrivi quali articoli vengono selezionati
+    articoli_con_and = Articolo.objects.filter(giornalista__anno_di_nascita__gt=data, visualizzazioni__gte=visualizzazioni)
+
+    from django.db.models import Q
+    #19. scrivi quali articoli vengono selezionati
+    articoli_con_or = Articolo.objects.filter(giornalista__anno_di_nascita__gt=data) | Q(visualizzazioni__lte=visualizzazioni)
+    
     #Dizionazio Contenxt
     context = {
         'articoli_cognome' : articoli_cognome,
@@ -141,6 +159,10 @@ def queryBase(request):
         'giornalista_anziano' : giornalista_anziano,
         'ultimi' : ultimi,
         'articoli_minime_visualizzazioni' : articoli_minime_visualizzazioni,
-        'articoli_parola' : articoli_parola
+        'articoli_parola' : articoli_parola,
+        'articoli_mese_anno' : articoli_mese_anno,
+        'giornalisti_con_articoli_popolari' : giornalisti_con_articoli_popolari,
+        'articoli_con_and' : articoli_con_and,
+        'articoli_con_or' : articoli_con_or
     }
     return render(request, 'news/query_base.html', context)
